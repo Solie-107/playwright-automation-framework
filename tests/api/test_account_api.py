2f7fdd75a9ae6ab07fc74ae2d6b55b514a5392e7
SALOMON MENIWABE <SALOMON@rafael.co.il>
2026-03-26 10:25:18 +0200

    Fix full account flow and signup login and Account api: handle google vignette + stable navigation + login flow

--- tests/api/test_account_api.py
+++ tests/api/test_account_api.py
@@ -1,4 +1,4 @@
-import pytest
+from flows.account_flow import AccountFlow
 from utils.data_factory import UserData
 
 
@@ -7,30 +7,10 @@ def test_verify_login_with_valid_credentials(
     page,
     user_data: UserData,
 ) -> None:
-    # Arrange - create user through UI first because API createAccount
-    # does not reliably create a real UI-loginable user in this site.
-    page.goto("https://automationexercise.com/login")
+    flow = AccountFlow(page, api_client)
 
-    page.locator("[data-qa='signup-name']").fill(user_data.name)
-    page.locator("[data-qa='signup-email']").fill(user_data.email)
-    page.locator("[data-qa='signup-button']").click()
-
-    page.locator("#id_gender1").check()
-    page.locator("[data-qa='password']").fill(user_data.password)
-    page.locator("[data-qa='days']").select_option("1")
-    page.locator("[data-qa='months']").select_option("1")
-    page.locator("[data-qa='years']").select_option("2000")
-    page.locator("[data-qa='first_name']").fill(user_data.first_name)
-    page.locator("[data-qa='last_name']").fill(user_data.last_name)
-    page.locator("[data-qa='company']").fill(user_data.company)
-    page.locator("[data-qa='address']").fill(user_data.address1)
-    page.locator("[data-qa='address2']").fill(user_data.address2)
-    page.locator("[data-qa='country']").select_option(user_data.country)
-    page.locator("[data-qa='state']").fill(user_data.state)
-    page.locator("[data-qa='city']").fill(user_data.city)
-    page.locator("[data-qa='zipcode']").fill(user_data.zipcode)
-    page.locator("[data-qa='mobile_number']").fill(user_data.mobile_number)
-    page.locator("[data-qa='create-account']").click()
+    # Arrange
+    flow.create_user_via_ui(user_data)
 
     # Act
     response = api_client.verify_login(
@@ -43,11 +23,7 @@ def test_verify_login_with_valid_credentials(
     assert str(response["body"].get("responseCode")) == "200"
 
     # Cleanup
-    delete_response = api_client.delete_account(
-        email=user_data.email,
-        password=user_data.password,
-    )
-    assert delete_response["status_code"] == 200
+    flow.delete_user_via_api(user_data)
 
 
 def test_get_user_detail_by_email(
@@ -55,29 +31,10 @@ def test_get_user_detail_by_email(
     page,
     user_data: UserData,
 ) -> None:
-    # Arrange - create via UI
-    page.goto("https://automationexercise.com/login")
+    flow = AccountFlow(page, api_client)
 
-    page.locator("[data-qa='signup-name']").fill(user_data.name)
-    page.locator("[data-qa='signup-email']").fill(user_data.email)
-    page.locator("[data-qa='signup-button']").click()
-
-    page.locator("#id_gender1").check()
-    page.locator("[data-qa='password']").fill(user_data.password)
-    page.locator("[data-qa='days']").select_option("1")
-    page.locator("[data-qa='months']").select_option("1")
-    page.locator("[data-qa='years']").select_option("2000")
-    page.locator("[data-qa='first_name']").fill(user_data.first_name)
-    page.locator("[data-qa='last_name']").fill(user_data.last_name)
-    page.locator("[data-qa='company']").fill(user_data.company)
-    page.locator("[data-qa='address']").fill(user_data.address1)
-    page.locator("[data-qa='address2']").fill(user_data.address2)
-    page.locator("[data-qa='country']").select_option(user_data.country)
-    page.locator("[data-qa='state']").fill(user_data.state)
-    page.locator("[data-qa='city']").fill(user_data.city)
-    page.locator("[data-qa='zipcode']").fill(user_data.zipcode)
-    page.locator("[data-qa='mobile_number']").fill(user_data.mobile_number)
-    page.locator("[data-qa='create-account']").click()
+    # Arrange
+    flow.create_user_via_ui(user_data)
 
     # Act
     response = api_client.get_user_detail_by_email(user_data.email)
@@ -93,11 +50,7 @@ def test_get_user_detail_by_email(
         assert returned_user["last_name"] == user_data.last_name
 
     # Cleanup
-    delete_response = api_client.delete_account(
-        email=user_data.email,
-        password=user_data.password,
-    )
-    assert delete_response["status_code"] == 200
+    flow.delete_user_via_api(user_data)
 
 
 def test_verify_login_with_invalid_password(
@@ -105,29 +58,10 @@ def test_verify_login_with_invalid_password(
     page,
     user_data: UserData,
 ) -> None:
-    # Arrange - create via UI
-    page.goto("https://automationexercise.com/login")
+    flow = AccountFlow(page, api_client)
 
-    page.locator("[data-qa='signup-name']").fill(user_data.name)
-    page.locator("[data-qa='signup-email']").fill(user_data.email)
-    page.locator("[data-qa='signup-button']").click()
-
-    page.locator("#id_gender1").check()
-    page.locator("[data-qa='password']").fill(user_data.password)
-    page.locator("[data-qa='days']").select_option("1")
-    page.locator("[data-qa='months']").select_option("1")
-    page.locator("[data-qa='years']").select_option("2000")
-    page.locator("[data-qa='first_name']").fill(user_data.first_name)
-    page.locator("[data-qa='last_name']").fill(user_data.last_name)
-    page.locator("[data-qa='company']").fill(user_data.company)
-    page.locator("[data-qa='address']").fill(user_data.address1)
-    page.locator("[data-qa='address2']").fill(user_data.address2)
-    page.locator("[data-qa='country']").select_option(user_data.country)
-    page.locator("[data-qa='state']").fill(user_data.state)
-    page.locator("[data-qa='city']").fill(user_data.city)
-    page.locator("[data-qa='zipcode']").fill(user_data.zipcode)
-    page.locator("[data-qa='mobile_number']").fill(user_data.mobile_number)
-    page.locator("[data-qa='create-account']").click()
+    # Arrange
+    flow.create_user_via_ui(user_data)
 
     # Act
     response = api_client.verify_login(
@@ -140,8 +74,4 @@ def test_verify_login_with_invalid_password(
     assert str(response["body"].get("responseCode")) != "200"
 
     # Cleanup
-    delete_response = api_client.delete_account(
-        email=user_data.email,
-        password=user_data.password,
-    )
-    assert delete_response["status_code"] == 200
+    flow.delete_user_via_api(user_data)
